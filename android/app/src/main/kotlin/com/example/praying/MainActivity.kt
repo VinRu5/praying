34ba8302af16accef093c641370ee19df3f35b81
Vincenzo Russo <vincenzorusso.inbox@gmail.com>
2023-06-23 15:50:12 +0200

    init

---
+++ android/app/src/main/kotlin/com/example/praying/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.praying
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:praying/app.dart';
+
+void main() {
+
+  runApp(const App());
+}
+
