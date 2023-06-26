42657df2515b8056c1e64cdb65cd0ecff5054430
Vincenzo Russo <vincenzorusso.inbox@gmail.com>
2023-06-26 16:22:04 +0200

    Add assets and images

--- lib/app.dart
+++ lib/app.dart
@@ -4,7 +4,32 @@ class App extends StatelessWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+  Widget build(BuildContext context) =>  MaterialApp(
+    home: Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment:  CrossAxisAlignment.center,
+        children: [
+          Text("Ciao",
+          style: TextStyle(
+            fontFamily: 'Nunito',
+          ),
+          ),
+          Text("Ciao",
+            style: TextStyle(
+              fontFamily: 'Nunito',
+              fontWeight: FontWeight.w900,
+            ),
+          ),
+          Text("Ciao",
+            style: TextStyle(
+              fontFamily: 'Ranchers',
+              fontWeight: FontWeight.w900,
+            ),
+          ),
+          Image.asset("assets/images/credo.png"),
+        ],
+      ),
+    ),
+  );
 }
